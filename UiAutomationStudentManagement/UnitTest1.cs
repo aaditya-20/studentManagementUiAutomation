db99c90d6cd591b7c10a6d7f5f86d7aa01c46996
aaditya-20 <91070772+aaditya-20@users.noreply.github.com>
2023-08-01 14:08:32 +0530

    Update UnitTest1.cs

--- UiAutomationStudentManagement/UnitTest1.cs
+++ UiAutomationStudentManagement/UnitTest1.cs
@@ -11,7 +11,12 @@ public class Tests
     [SetUp]
     public void Setup()
     {
-        Driver = new ChromeDriver();
+        var chromeOptions = new ChromeOptions();
+        //chromeOptions.BinaryLocation("/Applications/Google\Chrome.app/Contents/MacOS/Google\Chrome");
+
+        chromeOptions.AddArguments("headless");
+        chromeOptions.AddArguments("window-size=1920,1080");
+        Driver = new ChromeDriver(chromeOptions);
         Console.WriteLine("setup");
     }
 
